b461157539cfa37e73b439421820bcf79fbe07c2
radhad043 <166093686+radhad043@users.noreply.github.com>
2025-06-24 14:46:38 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://flipkart.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/FlipKart/);
 });
 
 test('get started link', async ({ page }) => {
